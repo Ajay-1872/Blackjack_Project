aefddf9203ff4280acb083cddcad3f6b2a83405c
Ajay-1872 <odedaaja@sheridancollege.ca>
2023-06-09 16:38:50 -0700

    Blackjack

---
+++ src/blackjackgame/BlackjackGame.java
@@ -0,0 +1,234 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package blackjackgame;
+
+/**
+ *
+ * @author ajay odedra
+ */
+import java.util.*;
+
+public class BlackjackGame {
+     List<Player> players;
+    Deck deck;
+    public boolean gameOver;
+
+    public BlackjackGame(int numPlayers) {
+        players = new ArrayList<>();
+        deck = new Deck();
+        gameOver = false;
+
+        for (int i = 0; i < numPlayers; i++) {
+            players.add(new Player("Player " + (i + 1)));
+        }
+    }
+
+    public void startGame() {
+        deck.shuffle();
+        dealInitialCards();
+
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s turn:");
+            playPlayerTurn(player);
+        }
+
+        playDealerTurn();
+
+        determineWinners();
+        gameOver = true;
+    }
+
+    private void dealInitialCards() {
+        for (Player player : players) {
+            player.receiveCard(deck.drawCard());
+        }
+        Card dealerCard = deck.drawCard();
+        dealerCard.setFaceUp(false);
+        players.get(0).receiveCard(dealerCard);
+    }
+
+    private void playPlayerTurn(Player player) {
+        while (!player.isBust() && player.getScore() < 21) {
+            System.out.println("Your hand: " + player.getHand());
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Do you want to hit or stand? (h/s): ");
+            String choice = scanner.nextLine().toLowerCase();
+
+            if (choice.equals("h")) {
+                player.receiveCard(deck.drawCard());
+            } else if (choice.equals("s")) {
+                break;
+            }
+        }
+    }
+
+    private void playDealerTurn() {
+        Player dealer = players.get(0);
+
+        while (!dealer.isBust() && dealer.getScore() < 17) {
+            dealer.receiveCard(deck.drawCard());
+        }
+
+        dealer.getHand().get(0).setFaceUp(true);
+        System.out.println("Dealer's hand: " + dealer.getHand());
+    }
+
+    private void determineWinners() {
+        Player dealer = players.get(0);
+        int dealerScore = dealer.getScore();
+        boolean dealerBust = dealer.isBust();
+
+        for (int i = 1; i < players.size(); i++) {
+            Player player = players.get(i);
+            int playerScore = player.getScore();
+            boolean playerBust = player.isBust();
+
+            if (playerBust) {
+                System.out.println(player.getName() + " is bust. Dealer wins!");
+            } else if (dealerBust) {
+                System.out.println("Dealer is bust. " + player.getName() + " wins!");
+            } else if (playerScore > dealerScore) {
+                System.out.println(player.getName() + " wins!");
+            } else if (playerScore < dealerScore) {
+                System.out.println(player.getName() + " loses. Dealer wins!");
+            } else {
+                System.out.println(player.getName() + " and Dealer have a tie!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of players: ");
+        int numPlayers = scanner.nextInt();
+
+        BlackjackGame game = new BlackjackGame(numPlayers);
+        game.startGame();
+    }
+}
+
+class Card {
+    private final String rank;
+    private final String suit;
+    private boolean faceUp;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+        faceUp = true;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        if (rank.equals("Ace")) {
+            return 11;
+        } else if (rank.equals("King") || rank.equals("Queen") || rank.equals("Jack")) {
+            return 10;
+        } else {
+            return Integer.parseInt(rank);
+        }
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    @Override
+    public String toString() {
+        if (faceUp) {
+            return rank + " of " + suit;
+        } else {
+            return "Face Down";
+        }
+    }
+}
+
+class Deck {
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+
+        String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+}
+
+class Player {
+    private final String name;
+    private final List<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+        hand = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void receiveCard(Card card) {
+        hand.add(card);
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public int getScore() {
+        int score = 0;
+        int numAces = 0;
+
+        for (Card card : hand) {
+            score += card.getValue();
+
+            if (card.getRank().equals("Ace")) {
+                numAces++;
+            }
+        }
+
+        while (score > 21 && numAces > 0) {
+            score -= 10;
+            numAces--;
+        }
+
+        return score;
+    }
+
+    public boolean isBust() {
+        return getScore() > 21;
+    }
+}
+
+
